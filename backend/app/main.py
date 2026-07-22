151599da7bc60a5102fa35e32a1e7a8e90a9976c
dimooon835 <dimooon835@gmail.com>
2026-07-22 20:44:03 +0300

    Создание диалога от 22.07.26

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,16 +1,25 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.config import settings
+from contextlib import asynccontextmanager
+from app.database import init_db
+from app import auth
 
-app = FastAPI(title=settings.app_name)
+@asynccontextmanager
+async def lifespan(_app: FastAPI):
+    init_db,
+    yield
+
+app = FastAPI(title=settings.app_name, lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[settings.frontend_url],
     allow_credentials=True,
     allow_methods=["GET", "POST", "DELETE"],
-    allow_headers=["Authorization", "Content-Type"]
-)
+    allow_headers=["Authorization", "Content-Type"])
+
+app.include_router(auth.router)
 
 @app.get("/health")
 def health() -> dict[str, str]:
